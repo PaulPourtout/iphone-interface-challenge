1bc1225cb310f5c8be2c2dfd7c6b0936f94042c3
PaulPourtout <paulpourtout@hotmail.com>
2016-09-19 17:15:50 +0200

    page d'accueil

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  // $("#menu-scroll-bot").hide();
+  $("#panel-bottom-btn-container").click(function(){
+    // $("#menu-scroll-bot").toggle("slide", "bottom", "slow");
+
+    $("#close-scroll-menu").slideToggle("slow");
+  });
+
+});
