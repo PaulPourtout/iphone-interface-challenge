3ea93e2219127d9013979df899d0cb5ad7d2b8a6
PaulPourtout <paulpourtout@hotmail.com>
2016-09-22 17:17:50 +0200

    repertoir fini + messages

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   $('#menu-scroll-bot').hide();
   $('.filtre-modal').hide();
+  $('#keyboard').hide();
 
   $("#panel-bottom-btn-container, #close-scroll-menu").click(function(){
     $("#menu-scroll-bot").slideToggle(500);
@@ -13,6 +14,32 @@ $(document).ready(function(){
 
   $('.fa-play').click(function(){
     $(this).toggleClass('fa-pause');
+    $('#music-title').children().toggleClass('hide');
   });
 
+  $('.container-airplay').click(function(){
+    $(this).children().toggleClass('airplay-active');
+  });
+
+  // APPARITION CLAVIER MESSAGES
+  var keyboard = false;
+  $('#writing-area').click(function(){
+    keyboard = true;
+    if (keyboard === true) {
+      $('#keyboard').slideDown(500);
+    }
+    else {
+      $('#keyboard').slideUp(500);
+    }
+  });
+
+  $('#messages-sent').click(function(){
+    keyboard = false;
+    $('#keyboard').slideUp(500)
+  });
+
+  // TOUCHE MAJUSCULE
+  $('.maj').click(function(){
+      $('.letter').toggleClass('toUppercase');
+  });
 });
